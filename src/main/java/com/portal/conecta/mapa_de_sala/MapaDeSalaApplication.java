24935ad1b785e31c3c3938b1f475eedec66fdd93
jonathan_uber <jonathanuber121207@gmail.com>
2026-05-18 18:42:13 -0300

    feat: create spring project

---
+++ src/main/java/com/portal/conecta/mapa_de_sala/MapaDeSalaApplication.java
@@ -0,0 +1,13 @@
+package com.portal.conecta.mapa_de_sala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapaDeSalaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapaDeSalaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portal/conecta/mapa_de_sala/MapaDeSalaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portal.conecta.mapa_de_sala;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapaDeSalaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
