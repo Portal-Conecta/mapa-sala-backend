a9b216126d016aa9e69640d7bdda7ad44cfb685b
jonathan_uber <jonathanuber121207@gmail.com>
2026-06-09 16:07:38 -0300

    #83 fix: corrige descrição do open api config (swagger)

--- src/main/java/com/portal/conecta/mapa_de_sala/shared/config/OpenApiConfig.java
+++ src/main/java/com/portal/conecta/mapa_de_sala/shared/config/OpenApiConfig.java
@@ -3,40 +3,30 @@ package com.portal.conecta.mapa_de_sala.shared.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.security.SecurityScheme.Type;
 
 @Configuration
 public class OpenApiConfig {
+
+    public static final String BEARER_AUTH_SCHEME = "bearerAuth";
 
 	@Bean
 	public OpenAPI customOpenAPI() {
 		return new OpenAPI()
 			.info(new Info()
-				.title("Portal Documental - Assessoria Imobiliária")
+				.title("Backend Mapa de Sala - Portal Conecta")
 				.description("""
-					Sistema de gestão documental para assessorias imobiliárias.
-					Fornece funcionalidades completas de autenticação, gerenciamento de usuários,
-					clientes, processos e propriedades imobiliárias com integração de controle de auditoria.
+					Backend do sistema de gerenciamento de mapas de sala para o Portal Conecta.
 					""")
-				.version("1.0.0")
-				.contact(new Contact()
-					.name("Suporte - Documental Assessoria")
-					.email("jonathanuberdev@gmail.com")
-					.url("https://documentalbrasil.com.br"))
-				.license(new License()
-					.name("Licença Proprietária")
-					.url("https://documentalbrasil.com.br")))
-			.components(new io.swagger.v3.oas.models.Components()
-				.addSecuritySchemes("Bearer Authentication",
-					new SecurityScheme()
-						.type(Type.HTTP)
+				.version("0.0.1-SNAPSHOT"))
+			.components(new Components()
+				.addSecuritySchemes(BEARER_AUTH_SCHEME, new SecurityScheme()
+						.name(BEARER_AUTH_SCHEME)
+						.type(SecurityScheme.Type.HTTP)
 						.scheme("bearer")
-						.bearerFormat("JWT")
-						.description("Token JWT para autenticação de requisições. Obtenha o token através do endpoint /auth/login.")));
+						.bearerFormat("JWT")));
 	}
 }
