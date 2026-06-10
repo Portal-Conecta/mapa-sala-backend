b6cce1fd9eddaef7326cd97d29f2e6d6304080ec
jonathan_uber <jonathanuber121207@gmail.com>
2026-06-10 15:26:42 -0300

    fix: deletea .gitkeep inútel e adicionar mais tipos no enum de histórico de mapa de sala 'RoomMapHistoryAction'

--- src/main/java/com/portal/conecta/mapa_de_sala/module/seat_map/domain/enums/RoomMapHistoryAction.java
+++ src/main/java/com/portal/conecta/mapa_de_sala/module/seat_map/domain/enums/RoomMapHistoryAction.java
@@ -1,7 +1,12 @@
 package com.portal.conecta.mapa_de_sala.module.seat_map.domain.enums;
 
 public enum RoomMapHistoryAction {
-    MAP_CREATION,
-    STUDENT_MOVED,
-    MAP_REPLICATED
+    MAP_CREATION, // mapa foi criado
+    MAP_REPLICATED, // mapa foi replicado
+    MAP_UPDATED, // mapa foi atualizado
+    MAP_DELETED, // mapa foi deletado
+    STUDENT_ASSIGNED, // aprendiz foi alocado para um lugar
+    STUDENT_MOVED,       // movimento simples ou parte de substituição
+    STUDENT_UNASSIGNED,  // perdeu lugar (DISPLACE)
+    STUDENTS_SWAPPED    // troca entre dois aprendizes
 }
