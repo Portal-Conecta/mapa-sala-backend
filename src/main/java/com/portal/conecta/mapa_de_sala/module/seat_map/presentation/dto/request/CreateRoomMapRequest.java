55f75b41fc45eeec016d63fa4ca68b82be4d110d
vinicius_zapella <vinicius_zapella@estudante.sesisenai.org.br>
2026-06-17 18:47:49 -0300

    refactor(#87): atualizar CreateRoomMapRequest e commands para alocacoes iniciais

--- src/main/java/com/portal/conecta/mapa_de_sala/module/seat_map/presentation/dto/request/CreateRoomMapRequest.java
+++ src/main/java/com/portal/conecta/mapa_de_sala/module/seat_map/presentation/dto/request/CreateRoomMapRequest.java
@@ -1,14 +1,12 @@
 package com.portal.conecta.mapa_de_sala.module.seat_map.presentation.dto.request;
 
 import jakarta.validation.constraints.NotNull;
+import java.util.List;
 import java.util.UUID;
 
 public record CreateRoomMapRequest(
-        @NotNull
-        UUID classId,
-        @NotNull
-        UUID roomId,
-        @NotNull
-        UUID layoutTemplateId
-) {
-}
+        @NotNull UUID classId,
+        @NotNull UUID roomId,
+        @NotNull UUID layoutTemplateId,
+        List<CreateRoomMapInitialAllocationRequest> locations
+) {}
